205f884d5953bb85637445438dd95cb445c89930
Jeff Cross <middlefloor@gmail.com>
2014-11-25 14:45:36 -0800

    feat(adapter): update adapter

--- src/adapter.js
+++ src/adapter.js
@@ -1,21 +1,86 @@
-(function(win) {
-  var benchmarks = [];
-  window.bench = function(config) {
-    console.log('registering');
-    benchmarks.push(config);
-    console.log('bench!', config);
-  };
+(function() {
 
-  function execute () {
-    console.log('executing!');
-    benchmarks.forEach(function (bench) {
-      console.log('bench: ', bench);
-    });
+//Config properties not to serialize to url
+var ignoredProps = ['url'];
+
+function BPAdapter (global) {
+  var self = this;
+  this.benchmarks = [];
+  this._global = global;
+  global.bpSuite = this.bpSuite.bind(this);
+}
+
+BPAdapter.prototype.bpSuite = function bpSuite(config) {
+  return this.runBenchmark.call(this, config);
+};
+
+BPAdapter.prototype.newWindow = function(config, done) {
+  var timer = setInterval(checkClosed.bind(this), 50);
+  var result;
+  this._benchWindow = this._global.open(
+    annotateUrl(config),
+    'benchpress',
+    [
+      'status=0',
+      'menubar=0',
+      'toolbar=0',
+      'location=0',
+      'personalbar=0',
+      'status=0',
+      'dependent=0',
+      'dialog=0'
+    ].join('&'));
+
+  this._benchWindow.addEventListener('benchpressComplete', function(e) {
+    result = e.result;
+  });
+
+  function checkClosed() {
+    if (!this._benchWindow || this._benchWindow.closed) {
+      clearInterval(timer);
+      done(result);
+    }
   }
+};
 
-  var createStartFn = function (karma) {
-    return execute;
-  };
+BPAdapter.prototype.closeWindow = function() {
+  this._benchWindow && this._benchWindow.close();
+  delete this._benchWindow;
+};
 
-  window.__karma__.start = createStartFn(window.__karma__);
-}(window))
+BPAdapter.prototype.runBenchmark = function(config) {
+  var self = this;
+  return new Promise(function(resolve) {
+    self.closeWindow();
+    self.newWindow(config, function(result) {
+      try {
+        resolve(result);
+      }
+      catch(e) {
+        console.error('could not resolve', e);
+      }
+
+    });
+  });
+};
+
+if (!window.__runningBenchPressAdapterSpec__) {
+  window.__bpAdapter__ = new BPAdapter(window);
+}
+else {
+  console.log('window.__runningBenchPressAdapterSpec__ truthy')
+}
+
+function annotateUrl(config) {
+  var urlParts = /([^\?^\#]*)([^\#]*)(.*)/.exec(config.url);
+  urlParts.shift();
+  //serialize config to search
+  urlParts[1] += (urlParts.length ? '&' : '') + '__bpAutoClose__=true';
+  Object.keys(config).forEach(function(prop) {
+    if (ignoredProps.indexOf(prop) > -1) return;
+    urlParts[1] += '&' + prop + '=' + config[prop];
+  });
+  return urlParts[0] + '?' + urlParts[1] + '#' + urlParts[2];
+}
+
+}());
